9f37c84679b1586b7871a0981a499ad8eaee711f
Jaroslaw Glod <57961700+Jarekgd@users.noreply.github.com>
2024-09-27 14:59:26 +0100

    up

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const firstName="Jaroslaw";
+const lastName="Glod";
+const title="Software Engineer"
+const about="<h3>About Me</h3>Thanks to different jobs, I have gained many valuable skills, like teamwork, communication with customers, and working under pressure. I pay close attention to detail and abstract and innovative thinking. My strengths are creativity, and the ability to learn quickly and cooperate without conflicts"
+
+document.getElementById("authorName").innerHTML=firstName+" "+lastName;
+document.getElementById("title").innerHTML=title;
+
+
+
